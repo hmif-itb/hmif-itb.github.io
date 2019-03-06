acd455db64ca6a88723f8c91da64d34a1981ab53
Yonas Adiel <yonazadielwiguna@gmail.com>
2019-03-07 05:38:38 +0700

    add quotes on home page

--- src/components/@Home/index.js
+++ src/components/@Home/index.js
@@ -1,21 +1,53 @@
 import React, { Component } from "react";
 import { Helmet } from "react-helmet";
+import QuoteSlideshow from "./QuoteSlideshow";
 import "./index.scss";
-import logoHmif from "assets/logo-hmif.png";
+
+const quotes = [
+  {
+    text: (
+      <span>
+        We nurture people to get internships. <br/>
+        We care. We build. <span className="big-underline">You achieve.</span>
+      </span>
+    ),
+    source: "Careers & Issues"
+  },
+  {
+    text: (
+      <span>
+        We build winners. <br/>
+        We build <span className="big-underline">champions</span>.
+      </span>
+    ),
+    source: "Competition & Community"
+  },
+  {
+    text: (
+      <span>
+        The best part of learning is <span className="big-underline">sharing</span> <br/>
+        what you know.
+      </span>
+    ),
+    source: "Vaughn K. Lauer"
+  },
+];
 
 export default class Home extends Component {
   render() {
     return (
       <div className="home">
         <Helmet>
-          <title> HMIF Tech </title>
+          <title>HMIF Tech</title>
         </Helmet>
 
         <div className="page welcome">
-            <img className="logo" src={logoHmif} alt="Logo HMIF" />
-            <h1 className="font-weight-bold text-center">#HMIF<span className="text-white">Tech</span></h1>
-            <div className="font-weight-bold text-center">Coming soon.</div>
-            <div className="border" />
+          <div className="tech mb-5">
+            <span className="font-weight-bold">#HMIF</span>
+            <span className="font-weight-thin">TECH</span>
+          </div>
+          <QuoteSlideshow quotes={quotes} />
+          <div className="register font-weight-semibold">let's act, <a href="//bit.ly/pemimpi-n">register here.</a></div>
         </div>
       </div>
     );
