0bad379913e994c1d193881f4aa7e7b64fdca1ff
Yonas Adiel <yonazadielwiguna@gmail.com>
2019-03-06 15:00:46 +0700

    add logo

--- src/components/@Home/index.js
+++ src/components/@Home/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Helmet } from "react-helmet";
 import "./index.scss";
+import logoHmif from "assets/logo-hmif.png";
 
 export default class Home extends Component {
   render() {
@@ -11,6 +12,8 @@ export default class Home extends Component {
         </Helmet>
 
         <div className="page welcome">
+            <div className="border" />
+            <img className="logo" src={logoHmif} alt="Logo HMIF" />
             <h1 className="font-weight-bold text-center">#HMIF<span className="text-white">Tech</span></h1>
             <div className="font-weight-bold text-center">Coming soon.</div>
         </div>
