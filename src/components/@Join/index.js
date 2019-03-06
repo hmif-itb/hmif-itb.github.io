9972c03f42c9e03aa4c8df069c752c7cc9a812d3
Yonas Adiel <yonazadielwiguna@gmail.com>
2019-03-06 15:40:59 +0700

    fix 404 on routing

--- src/components/@Join/index.js
+++ src/components/@Join/index.js
@@ -13,9 +13,9 @@ export default class Home extends Component {
         <div className="page join">
             <h1 className="font-weight-bold text-center">Join #HMIF<span className="text-white">Tech</span>!</h1>
             <div className="text-center font-italic">Dare to contribute?</div>
-            <div className="button font-weight-bold">
-                <a href="https://bit.ly/pemimpi-n">Click here!</a>
-            </div>
+            <a href="https://bit.ly/pemimpi-n">
+                <div className="button font-weight-bold">Click here!</div>
+            </a>
             <div className="border" />
         </div>
       </div>
