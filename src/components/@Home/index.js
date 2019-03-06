7ae3f3068f7f9718ebd4ca9eb664080855bb9afe
Yonas Adiel <yonazadielwiguna@gmail.com>
2019-03-06 14:39:32 +0700

    add styles and create coming soon page

--- src/components/@Home/index.js
+++ src/components/@Home/index.js
@@ -2,15 +2,18 @@ import React, { Component } from "react";
 import { Helmet } from "react-helmet";
 import "./index.scss";
 
-export default class Olympus extends Component {
+export default class Home extends Component {
   render() {
     return (
-      <div class="olympus">
+      <div className="home">
         <Helmet>
           <title> HMIF Tech </title>
         </Helmet>
 
-        Coming soon.
+        <div className="page welcome">
+            <h1 className="font-weight-bold text-center">#HMIF<span className="text-white">Tech</span></h1>
+            <div className="font-weight-bold text-center">Coming soon.</div>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import createBrowserHistory from 'history/createBrowserHistory';
 
 import * as serviceWorker from './serviceWorker';
 import Home from 'components/@Home';
-import 'components/index.scss';
-import 'components/spacing.scss';
-import 'components/text.scss';
+import 'styles/index.scss';
 
 const history = createBrowserHistory();
 
