24bf7c0816a3f41135d580809793ae38fd96605b
Yonas Adiel <yonazadielwiguna@gmail.com>
2019-03-11 08:10:23 +0700

    add Game page

--- src/components/@Bootcamp/@Game/index.js
+++ src/components/@Bootcamp/@Game/index.js
@@ -5,63 +5,37 @@ import { Link } from 'react-router-dom';
 import { ROUTE_BOOTCAMP } from 'routes';
 import Photo from 'components/Photo';
 import Header from '../Header';
-import LogoDS from 'assets/logo/ds.png';
-import Christian from 'assets/photos/christian.png';
+import LogoGame from 'assets/logo/ds.png';
+import Fiqri from 'assets/photos/fiqri.png';
 
 import './index.scss';
 
-export default class BootcampDS extends Component {
+export default class BootcampGame extends Component {
   render() {
     return (
-      <div className="bootcamp-ds">
+      <div className="bootcamp-game">
         <Helmet>
-          <title> Bootcamp HMIF - Data Science </title>
+          <title> Bootcamp HMIF - Game Development </title>
         </Helmet>
 
         <Header
-          logo={LogoDS}
-          title="Data Science"
-          quote="Data represent billion stories to tell. It's our job to give them a voice." />
+          logo={LogoGame}
+          title="Game Development"
+          quote="" />
 
         <div className="page container">
           <div className="row d-flex flex-column align-items-center py-5">
-            <Photo className="inverted" imageSrc={Christian} size="large" name="Christian Wibisono" title="Head of Data Science Community" />
-          </div>
-
-          <div className="row mb-3">
-            <div className="col-12">
-              <h2 className="mb-3">What is Data Science?</h2>
-              <p>
-              Data Science bootcamp is a 3-week event that contains discussion and hands-on about basic data science.
-              Hosted by speakers with various experience in data science competition, participants will get a variety of new knowledge about data science.
-              </p>
-              <p className="text-small">
-                <span className="font-italic">The goal is to turn data into information, and information into insight.</span> ~ Carly Fiorina, former CEO, Hewlett-Packard Co
-              </p>
-            </div>
+            <Photo className="inverted" imageSrc={Fiqri} size="large" name="M Fiqri Fatriansyah" title="Head of Game Development Community" />
           </div>
 
           <div className="row mb-3">
             <div className="col-12">
               <h2 className="mb-3">What you will learn</h2>
               <ul className="ml-4">
-                <li>Introduction to Data Science</li>
-                <li>Data Science Workflow</li>
-                <li>Exploratory Data Analysis</li>
-                <li>Feature Engineering 101</li>
-                <li>Supervised Learning</li>
-                <li>Unsupervised Learning</li>
-              </ul>
-            </div>
-          </div>
-
-          <div className="row mb-3">
-            <div className="col-12">
-              <h2 className="mb-3">Preferrable Skills</h2>
-              <ul className="ml-4">
-                <li>Programming language (Python)</li>
-                <li>Passion in big data</li>
-                <li>Curious to learn new things and explore creative solutions</li>
+                <li>Game Mechanics with Unity</li>
+                <li>Game Flow Design</li>
+                <li>UI / UX of Game</li>
+                <li>Game Development Team Building</li>
               </ul>
             </div>
           </div>
