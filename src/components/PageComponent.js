b4873a08c7eefe02893d0fe65e6aedd68c45498d
Yonas Adiel <yonazadielwiguna@gmail.com>
2019-09-13 12:49:33 +0700

    add gtag

--- src/components/PageComponent.js
+++ src/components/PageComponent.js
@@ -1,7 +1,9 @@
 import { Component } from "react";
+import ReactGA from 'react-ga';
 
 export default class PageComponent extends Component {
   componentDidMount() {
+    ReactGA.pageview(window.location.pathname + window.location.search);
     window.scrollTo(0, 0);
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
 
 import * as serviceWorker from './serviceWorker';
 import Router from 'routes/router';
 import 'styles/index.scss';
 
+ReactGA.initialize('UA-141178655-2');
 ReactDOM.render(
 	<BrowserRouter>
 		<Router />
