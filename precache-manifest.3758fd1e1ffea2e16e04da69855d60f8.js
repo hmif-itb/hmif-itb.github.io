679e6176d6777ee7cb08702e2e0a44afb3b10b96
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2020-02-25 18:00:36 +0700

    Updates

---
+++ precache-manifest.3758fd1e1ffea2e16e04da69855d60f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dad90aabefec64e50e77",
+    "url": "/static/css/main.4b414fe0.chunk.css"
+  },
+  {
+    "revision": "dad90aabefec64e50e77",
+    "url": "/static/js/main.dad90aab.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "cfebe25b8e0f54bb5390",
+    "url": "/static/js/2.cfebe25b.chunk.js"
+  },
+  {
+    "revision": "5219dcfcc32f2252c03dfe0532fa7af8",
+    "url": "/static/media/qmarkbg.5219dcfc.svg"
+  },
+  {
+    "revision": "511ab420e84911a6e36c6812c3e89ee3",
+    "url": "/index.html"
+  }
+];
