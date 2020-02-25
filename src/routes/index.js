14f4c77f9623950efae55e280c224c557c115e6b
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2020-02-25 17:51:02 +0700

    Remove unused assets

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,1 @@
 exports.ROUTE_HOME = '/';
-exports.ROUTE_ABOUT = '/about';
-exports.ROUTE_COMPETITION_SHARING = '/CompeteAndShare';
-
-exports.ROUTE_BOOTCAMP = '/bootcamp';
-exports.ROUTE_BOOTCAMP_REGISTER = exports.ROUTE_BOOTCAMP + '/register';
-exports.ROUTE_BOOTCAMP_BP = exports.ROUTE_BOOTCAMP + '/business-plan';
-exports.ROUTE_BOOTCAMP_CTF = exports.ROUTE_BOOTCAMP + '/capture-the-flag';
-exports.ROUTE_BOOTCAMP_CP = exports.ROUTE_BOOTCAMP + '/competitive-programming';
-exports.ROUTE_BOOTCAMP_DS = exports.ROUTE_BOOTCAMP + '/data-science';
-exports.ROUTE_BOOTCAMP_GAME = exports.ROUTE_BOOTCAMP + '/game-development';
-exports.ROUTE_BOOTCAMP_UX = exports.ROUTE_BOOTCAMP + '/ui-ux';
