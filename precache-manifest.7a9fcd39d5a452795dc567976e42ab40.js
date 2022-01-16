688f621f43c11776b9e6cb6e42b2196207cc6591
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2022-01-16 17:28:14 +0700

    Revert "Updates"
    This reverts commit 4f3c00d28f97730982399e946ce06e6ad0b695c7.

---
+++ precache-manifest.7a9fcd39d5a452795dc567976e42ab40.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d7582155c8221211bfe",
+    "url": "/static/css/main.098cfcab.chunk.css"
+  },
+  {
+    "revision": "2d7582155c8221211bfe",
+    "url": "/static/js/main.2d758215.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "b5ce0ed9ab54456ea333",
+    "url": "/static/js/2.b5ce0ed9.chunk.js"
+  },
+  {
+    "revision": "aea6c8a56a1a83d3cc686ae2fede21e3",
+    "url": "/static/media/cp.aea6c8a5.svg"
+  },
+  {
+    "revision": "7aef08f8252cb962d1e0dd26af4cabe7",
+    "url": "/static/media/ctf.7aef08f8.svg"
+  },
+  {
+    "revision": "1d6357f61b767891fea8b48b19403c34",
+    "url": "/static/media/ds.1d6357f6.svg"
+  },
+  {
+    "revision": "7791c0875f33bc395175bc528f8d9493",
+    "url": "/static/media/gd.7791c087.svg"
+  },
+  {
+    "revision": "6dc2cc7d8efc2421c4bafa4dce728273",
+    "url": "/static/media/ux.6dc2cc7d.svg"
+  },
+  {
+    "revision": "5ae0473fd16b9bf27e889b17f5ee1fd8",
+    "url": "/static/media/bootcamp.5ae0473f.png"
+  },
+  {
+    "revision": "5219dcfcc32f2252c03dfe0532fa7af8",
+    "url": "/static/media/qmarkbg.5219dcfc.svg"
+  },
+  {
+    "revision": "d70e9c5a0c890b686a39a77ff56aac50",
+    "url": "/index.html"
+  }
+];
